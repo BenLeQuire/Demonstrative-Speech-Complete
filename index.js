471ca1dc0c00c60ea0e34920ef018571fcb693fa
Ben LeQuire <101414216+BenLeQuire@users.noreply.github.com>
2025-10-10 19:51:22 -0400

    create repository

---
+++ index.js
@@ -0,0 +1,8 @@
+const button =document.getElementById("submit-button");
+
+function submit () {
+    //console.log("y");
+    window.open("https://www.example.com")
+}
+
+button.addEventListener("click", submit);
