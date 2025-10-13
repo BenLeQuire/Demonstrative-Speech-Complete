53dc8a4aef601e159114ede8375190456f1ae44d
Ben LeQuire <101414216+BenLeQuire@users.noreply.github.com>
2025-10-13 09:38:27 -0400

    updates!!!!!

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const button =document.getElementById("submit-button");
 
 function submit () {
+    let name =document.getElementById("name").value;
+    let question = document.getElementById("question").value;
+    alert("hello, your name is " + name +" and your question is " + question);
     //console.log("y");
-    window.open("https://www.example.com")
+    //window.open("https://www.example.com")
 }
 
 button.addEventListener("click", submit);
